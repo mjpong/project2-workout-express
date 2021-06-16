1a0e6c15ed906abcbe77386d7532c419ae7fc755
mjpong <jolenepong@gmail.com>
2021-06-16 06:27:35 +0000

    db collections added and can connect

--- MongoUtil.js
+++ MongoUtil.js
@@ -1,21 +1,25 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 let _db;
-async function connect(url, dbname) {
+
+async function connect(url, dbName) {
+
+    // create a client
     let client = await MongoClient.connect(url, {
         useUnifiedTopology:true
     })
     
-    // function(err, client){
-    //     console.log("server connected");
-    //     _db = client.db(dbname);
-    // })
-    _db = client.db(dbname);
-    console.log("connected")
+    // use a database
+    _db = client.db(dbName);
+    console.log("connected");   
 }
+
 function getDB() {
     return _db;
 }
+
+// exporting out the connect function
 module.exports = {
     connect, getDB
 }
+
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+// setup express app start
+
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const MongoUtil= require('./MongoUtil');
-MongoUtil.connect(process.env.MONGO_URL, 'workout')
+const mongoURL = process.env.MONGO_URL
+
+// setup express
+let app = express();
+
+// enable processing json
+app.use(express.json());
+
+//enable cors
+app.use(cors())
+
+
+async function main() {
+
+    //connect to mongodb
+    let db = await MongoUtil.connect(mongoURL, 'workout')
+
+    // add routes
+    app.get('/', function (req, res){
+        res.send("<h1>Hello from Workout Express</h1>")
+    })
+
+    app.get('/create', async function(req, res){
+        db = MongoUtil.getDB();
+        res.send(await db.collection("muscle").find().toArray())
+    })
+
+    
+
+    
+}
+
+main()
+
+// start server
+app.listen(3000, () => {
+    console.log("Server has started")
+})
